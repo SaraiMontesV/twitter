fd6fef5b9e47469998a4bb7c9fb69d0abe95746c
SaraiMV <sarizvargas@gmail.com>
2022-04-20 21:23:33 -0500

    Modifying unit test for case 4 (setters)

--- test/models/User.test.js
+++ test/models/User.test.js
@@ -15,11 +15,16 @@ describe("Unit Test ", () => {
   test("Case 3: Adding getters", () => {
     const user = new User(1, "Sarai", "SaraiMV", "Bio");
     expect(user.getUsername).toBe("Sarai");
+    expect(user.getBio).toBe("Bio");
+    expect(user.getDateCreated).not.toBeUndefined();
+    expect(user.lastUpdated).not.toBeUndefined();
   });
 
   test("Case 4: Adding setters", () => {
     const user = new User(1, "Sarai", "SaraiMV", "Bio");
     user.setUsername = "Sara";
+    user.setBio = "New bio";
     expect(user.username).toBe("Sara");
+    expect(user.bio).toBe("New bio");
   });
 });
