ca46a9c760aafaa8dcb9c2191e2684e5c87991a7
SaraiMV <sarizvargas@gmail.com>
2022-04-20 22:12:09 -0500

    Adding static method updateUserUsername

--- app/services/UserService.js
+++ app/services/UserService.js
@@ -5,8 +5,13 @@ class UserService {
     return new User(id, username, name, "Sin bio");
   }
 
-  static getInfo(user){
-      return [user.id, user.username, user.name, user.bio]
+  static getInfo(user) {
+    return [user.id, user.username, user.name, user.bio];
+  }
+
+  static updateUserUsername(user, newUsername) {
+    user.username = newUsername;
+    return user;
   }
 }
 
