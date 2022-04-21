b360bbda472dbcc4572bc89d79cad59ae3e79065
SaraiMV <sarizvargas@gmail.com>
2022-04-20 20:55:33 -0500

    Modifying test User for case 2

--- test/models/User.test.js
+++ test/models/User.test.js
@@ -1,9 +1,14 @@
 const User = require("./../../app/models/User");
 
 describe("Unit Test ", () => {
-  test("Case 1", () => {
-    const user = new User(1, "Sarai", "Carlo", "Bio");
+  test("Case 1: Create User", () => {
+    const user = new User(1, "Sarai", "SaraiMV", "Bio");
     expect(user.id).toBe(1);
     expect(user.username).toBe("Sarai");
+  });
+
+  test("Case 2: Dates in attributes User", () => {
+    const user = new User(1, "Sarai", "SaraiMV", "Bio");
+    expect(user.dateCreated).not.toBeUndefined();
   });
 });
